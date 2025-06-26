c7b86726a7098c08a798db6bc9efefd2d5c1fc71
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-06-26 10:24:42 +0200

    [ADD] Money value converter

---
+++ Numismatics.WPF/ViewModels/Converters/DecimalConverter.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace Numismatics.WPF.ViewModels.Converters
+{
+    public class DecimalConverter : IValueConverter
+    {
+        private readonly CultureInfo culture = new CultureInfo("sr-SR");
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            try
+            {
+                double number;
+
+                if (value is double d)
+                    number = d;
+                else if (value is string s && double.TryParse(s, NumberStyles.Any, CultureInfo.InvariantCulture, out var parsed))
+                    number = parsed;
+                else
+                    return value?.ToString();
+
+                return number.ToString("N2", this.culture);
+            }
+            catch
+            {
+                return value?.ToString();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return Binding.DoNothing;
+        }
+    }
+
+}
