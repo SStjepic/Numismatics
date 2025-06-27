70d30e78a9d53491af5d82994e0b68a11680a548
Stefan Stjepic <stefan.stjepic03@gmail.com>
2025-06-27 12:30:31 +0200

    [BUGFIX] Function UpdateByCoin

--- Numismatics.INFRASTRUCTURE/Repositories/SQLite/SQLiteOwnedCoinRepository.cs
+++ Numismatics.INFRASTRUCTURE/Repositories/SQLite/SQLiteOwnedCoinRepository.cs
@@ -59,15 +59,20 @@ namespace Numismatics.INFRASTRUCTURE.Repositories.SQLite
 
         public List<OwnedCoin> UpdateByCoin(long coinId, List<OwnedCoin> coins)
         {
-            var existing = _context.OwnedCoins.Where(o => o.CoinId == coinId).ToList();
-            _context.OwnedCoins.RemoveRange(existing);
-
-            foreach (var coin in coins)
+            var newCoins = coins.Where(c => c.Id == 0).ToList();
+            foreach (var newOwned in newCoins)
             {
-                _context.OwnedCoins.Add(coin);
+                newOwned.Id = DateTime.Now.Ticks;
             }
-
+            _context.AddRange(newCoins);
             _context.SaveChanges();
+
+            var banknoteIds = coins.Where(c => c.Id != 0).Select(c => c.Id).ToHashSet();
+            var toRemove = _context.OwnedCoins.Where(e => !banknoteIds.Contains(e.Id)).ToList();
+
+            _context.OwnedCoins.RemoveRange(toRemove);
+            _context.SaveChanges();
+
             return coins;
         }
     }
